cf31452f6cfd407f43583c675e08193bfbc788af
frontendprof <https://github.com/frontendprof>
2021-02-12 23:43:26 +0500

    Campground update and edit and delete

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express=require("express")
 const path=require("path");
 const mongoose=require("mongoose");
+const methodOverride=require("method-override")
 const Campground=require('./models/campground')
 
 
@@ -29,6 +30,7 @@ app.set("view engine", 'ejs')
 app.set("views", path.join(__dirname,'views'))
 
 app.use(express.urlencoded({extended:true}))
+app.use(methodOverride('_method'))
 
 
 app.get("/",(req,res)=>{
@@ -56,6 +58,23 @@ app.get("/campgrounds/:id",async(req,res)=>{
     res.render("campgrounds/show",{campground})
 })
 
+app.get("/campgrounds/:id/edit",async(req,res)=>{
+    const campground=await Campground.findById(req.params.id);
+    res.render("campgrounds/edit",{campground})
+})
+
+
+app.put("/campgrounds/:id",async(req,res)=>{
+    const {id}=req.params;
+    const campground=await Campground.findByIdAndUpdate(id,{...req.body.campground})
+    res.redirect(`/campgrounds/${campground._id}`)
+})
+
+app.delete("/campgrounds/:id",async(req,res)=>{
+    const {id}=req.params;
+    await Campground.findByIdAndDelete(id)
+    res.redirect("/campgrounds")
+})
 
 
 
