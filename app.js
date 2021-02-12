ae2b94b77f01d2ed17a2490a9be8be56b05061bd
frontendprof <https://github.com/frontendprof>
2021-02-12 11:52:42 +0500

    Successful database connection

--- app.js
+++ app.js
@@ -1,13 +1,42 @@
 const express=require("express")
+const path=require("path");
+const mongoose=require("mongoose");
+const Campground=require('./models/campground')
+
+
+
+
+mongoose.connect('mongodb://localhost:27017/panjob-camp',{
+    useNewUrlParser:true,
+    useCreateIndex:true,
+    useUnifiedTopology:true
+});
+
+
+const db=mongoose.connection;
+db.on("error", console.log.bind(console, "connection error:"));
+db.once("open", ()=>{
+    console.log("Database connected") 
+});
+
+
+
+
 const app=express();
 
 
+app.set("view engine", 'ejs')
+app.set("views", path.join(__dirname,'views'))
 
 
-
+app.get("/makecampground",async(req,res)=>{
+    const camp=new Campground({title:"My Backyard",description:"Relaxing camping"})
+    await camp.save();
+    res.send(camp)
+})
 
 app.get("/",(req,res)=>{
-    res.send("Hello from Panjob Camp")
+    res.render('home')
 })
 
 
