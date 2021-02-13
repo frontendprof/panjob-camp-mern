c14b113a7c3ff1f16c44aaeec21985aa386284c9
frontendprof <https://github.com/frontendprof>
2021-02-13 11:42:20 +0500

    Adding images

--- seeds/index.js
+++ seeds/index.js
@@ -31,9 +31,13 @@ const seedDB=async()=>{
     for(let i=0;i<50;i++){
 
         const random1000=Math.floor(Math.random()*1000);
+        const price=Math.floor(Math.random()*20)+10;
         const camp=new Campground({
         location:`${cities[random1000].city}, ${cities[random1000].state}`,
-        title:`${sample(descriptors)}, ${sample(places)}`
+        title:`${sample(descriptors)}, ${sample(places)}`,
+        image:"https://source.unsplash.com/collection/483251",
+        description:"Lorem ipsum dolor sit amet consectetur adipisicing elit. Excepturi beatae accusamus dolorem modi nam reprehenderit necessitatibus iusto ipsum!",
+        price
     })
         await camp.save();
 
